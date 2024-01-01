7acd9bd5630c729ae97373a0c983c509c9355a92
corradiniste <corradiniste@users.noreply.github.com>
2024-01-01 17:44:19 +0100

    Update censor.py

--- modules/censor.py
+++ modules/censor.py
@@ -1,26 +1,25 @@
-# modified version of https://github.com/AUTOMATIC1111/stable-diffusion-webui-nsfw-censor/blob/master/scripts/censor.py
-
-import numpy as np
 import torch
-import modules.core as core
-
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
 from PIL import Image
-import modules.config
+
+from modules import scripts, shared
 
 safety_model_id = "CompVis/stable-diffusion-safety-checker"
 safety_feature_extractor = None
 safety_checker = None
 
 
-def numpy_to_pil(image):
-    image = (image * 255).round().astype("uint8")
+def numpy_to_pil(images):
+    """
+    Convert a numpy image or a batch of images to a PIL image.
+    """
+    if images.ndim == 3:
+        images = images[None, ...]
+    images = (images * 255).round().astype("uint8")
+    pil_images = [Image.fromarray(image) for image in images]
 
-    #pil_image = Image.fromarray(image, 'RGB')
-    pil_image = Image.fromarray(image)
-
-    return pil_image
+    return pil_images
 
 
 # check and replace nsfw content
@@ -28,8 +27,8 @@ def check_safety(x_image):
     global safety_feature_extractor, safety_checker
 
     if safety_feature_extractor is None:
-        safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id, cache_dir=modules.config.path_safety_checker_models)
-        safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id, cache_dir=modules.config.path_safety_checker_models)
+        safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
+        safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
 
     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
     x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
@@ -37,19 +36,21 @@ def check_safety(x_image):
     return x_checked_image, has_nsfw_concept
 
 
-def censor_single(x):
-    x_checked_image, has_nsfw_concept = check_safety(x)
+def censor_batch(x):
+    x_samples_ddim_numpy = x.cpu().permute(0, 2, 3, 1).numpy()
+    x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim_numpy)
+    x = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
-    # replace image with black pixels, keep dimensions
-    # workaround due to different numpy / pytorch image matrix format
-    if has_nsfw_concept[0]:
-        imageshape = x_checked_image.shape
-        x_checked_image = np.zeros((imageshape[0], imageshape[1], 3), dtype = np.uint8)
-
-    return x_checked_image
+    return x
 
 
-def censor_batch(images):
-    images = [censor_single(image) for image in images]
+class NsfwCheckScript(scripts.Script):
+    def title(self):
+        return "NSFW check"
 
-    return images
+    def show(self, is_img2img):
+        return scripts.AlwaysVisible
+
+    def postprocess_batch(self, p, *args, **kwargs):
+        images = kwargs['images']
+        images[:] = censor_batch(images)[:]
